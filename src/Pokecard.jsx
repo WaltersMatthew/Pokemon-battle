24336baafe270840565f58478f95045cad2ae37b
waltersmatthew <waltersmatthew88@gmail.com>
2023-03-09 17:02:14 -0800

    insert logic for exp totals and random picks

--- src/Pokecard.jsx
+++ src/Pokecard.jsx
@@ -1,8 +1,15 @@
 export default function Pokecard(props){
+    
+    const urlFixer = (id) => {
+        if(id < 10) id = `00${id}`
+        else if(id < 100)  id = `0${id}`
+        return id
+    }
+
     return(
             <div className={props.type}>
                 <h1>{props.name}</h1>
-                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`} alt={props.name} />
+                <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${urlFixer(props.id)}.png`} alt={props.name} />
                 <p>Type: {props.type}</p>
                 <p>base experience: {props.base_experience}</p>
             </div>
--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -15,7 +15,35 @@ const data = [
     {id: 143, name: 'Snorlax', type: 'normal', base_experience: 189}
   ]
 
-const pokeMap = data.map(item =>{
+const handOne = []
+const handTwo = []
+
+function randomNoRepeats(array) {
+    var copy = array.slice(0);
+    return function() {
+      if (copy.length < 1) { copy = array.slice(0); }
+      var index = Math.floor(Math.random() * copy.length);
+      var item = copy[index];
+      copy.splice(index, 1);
+      return item;
+    };
+  }
+const chooser = randomNoRepeats(data)
+
+for(let i = 0; i < 8; i++){  
+    if(i%2) handOne.push(chooser())
+    else handTwo.push(chooser())
+}
+let handOneTotal = 0
+let handTwoTotal = 0
+
+for(let i = 0; i < handOne.length; i++){
+    handOneTotal += handOne[i].base_experience
+    handTwoTotal += handTwo[i].base_experience
+}
+
+
+const playerOneMap = handOne.map(item =>{
     return (
         <Pokecard
             key={item.id}
@@ -26,13 +54,28 @@ const pokeMap = data.map(item =>{
             base_experience = {item.base_experience}
         />
     )
-}
-)
+})
+const playerTwoMap = handTwo.map(item =>{
+    return (
+        <Pokecard
+            key={item.id}
+            className={item.type}
+            id = {item.id}
+            name = {item.name}
+            type= {item.type}
+            base_experience = {item.base_experience}
+        />
+    )
+})
 export default function Pokedex(){
     return(
     <>
-        <h1>Pokedex</h1>
-        <div className="card-block">{pokeMap}</div>
+        <h1>Player One</h1>
+        <h2>Total Points: {handOneTotal}</h2>
+        <div className="card-block">{playerOneMap}</div>
+        <h1>Player Two</h1>
+        <h2>Total Points: {handTwoTotal}</h2>
+        <div className="card-block">{playerTwoMap}</div>
     </>
     )
 }
