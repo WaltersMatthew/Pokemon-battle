adc3379002aee493e4557bce36e0cc24f80e11f5
waltersmatthew <waltersmatthew88@gmail.com>
2023-04-05 09:16:17 -0700

    finish with data and maps. On to styling

--- src/Poke.jsx
+++ src/Poke.jsx
@@ -29,18 +29,72 @@ function Poke() {
             return <li key={ability.slot}>{ability.ability.name}</li>;
         });
 
-    const statMap = {};
+    const statMap =
+        pokeData &&
+        pokeData.stats.map((stat) => {
+            return (
+                <li key={stat.stat.url}>
+                    {stat.stat.name}: {stat.base_stat}
+                </li>
+            );
+        });
+
+    const typeMap =
+        pokeData &&
+        pokeData.types.map((type) => {
+            return <p key={type.type.url}>{capitalizer(type.type.name)}</p>;
+        });
 
     return (
         <div>
-            <h1>one poke</h1>
             <h1>{pokeData && capitalizer(pokeData.name)}</h1>
-            <img
-                src={pokeData && pokeData.sprites.front_default}
-                height="200px"
-                alt={pokeData && pokeData.name}
-            />
-            <ul>{abilityMap}</ul>
+            <div className="types">
+                <h2>{typeMap}</h2>
+            </div>
+            <div className="height-weight">
+                <p>Height: {pokeData && pokeData.height * 10}cm</p>
+                <p>Weight: {pokeData && pokeData.weight / 10}kg</p>
+            </div>
+            <div className="sprites">
+                <h1>Various Sprites</h1>
+                <img
+                    src={pokeData && pokeData.sprites.front_default}
+                    height="200px"
+                    alt={pokeData && pokeData.name}
+                />
+                <img
+                    src={pokeData && pokeData.sprites.back_default}
+                    height="200px"
+                    alt={pokeData && pokeData.name}
+                />
+                <img
+                    src={pokeData && pokeData.sprites.other.home.front_default}
+                    height="200px"
+                    alt={pokeData && pokeData.name}
+                />
+                <img
+                    src={
+                        pokeData &&
+                        pokeData.sprites.other["official-artwork"].front_default
+                    }
+                    height="200px"
+                    alt={pokeData && pokeData.name}
+                />
+                <img
+                    src={
+                        pokeData &&
+                        pokeData.sprites.other.dream_world.front_default
+                    }
+                    height="200px"
+                    alt={pokeData && pokeData.name}
+                />
+            </div>
+            <div className="stats">
+                <ul>{statMap}</ul>
+            </div>
+            <div className="abilities">
+                <ul>{abilityMap}</ul>
+            </div>
         </div>
     );
 }
